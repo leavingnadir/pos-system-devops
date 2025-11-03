bd947ca721d7d0b160f7dfb4d4d9fa5c916d04ef
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-11-03 23:09:13 +0530

    Complete Models

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,20 +2,41 @@ const User = require('../model/UserSchema');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const signup = async (req, resp) => {
     try {
         const {fullName, email, passwordHash} = req.body;
         const existingUser = await User.findOne({email});
-        if (existingUser) return resp.status(409).json({message: 'User Already exists'});
+        if (existingUser) return resp.status(409).json({'message': 'User Already exists'});
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const savedUser = await User.create({fullName, email, hashedPassword});
+        resp.status(201).json({'message': 'User Signed Up Successfully', data: savedUser});
+
     } catch (e) {
-        resp.status(500).json({message: 'SignUp Error',error:e});
+        resp.status(500).json({'message': 'SignUp Error',error:e});
     }
 };
 
 const login = async (req, resp) => {
     try {
+        const {email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (!existingUser) return resp.status(404).json({'message': 'User Not Found'});
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser.passwordHash);
+        if (!isPasswordValid) return resp.status(401).json({'message': 'Invalid Credentials'});
+
+        const token = jwt.sign({email: existingUser.email}, JWT_SECRET, {expiresIn: '10h'});
+        resp.status(200).json({'message' : 'Login Successful', token:token});
 
     } catch (e) {
-        resp.status(500).json({message: 'SignUp Error',error:e});
+        resp.status(500).json({'message': 'SignUp Error',error:e});
     }
+};
+
+module.exports = {
+    signup,
+    login
 };
